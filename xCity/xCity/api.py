9e9291ab5162177a2eb04f833ae4542fecae60ab
Mustafa <mustafa.ezzat@backstage.com>
2024-08-16 18:21:19 +0300

    ceate and use AuthBearer for now

--- xCity/xCity/api.py
+++ xCity/xCity/api.py
@@ -1,8 +1,13 @@
 from ninja import NinjaAPI
 from developer.api import router as developer_router
 from project.api import router as project_router
+from .AuthBearer import AuthBearer
 
 api = NinjaAPI(title="xCity", description="Api docs for real estate project.", csrf=True)
 
 api.add_router('/developers', developer_router)
 api.add_router('/projects', project_router)
+
+@api.get("/bearer", auth=AuthBearer())
+def bearer(request):
+    return {"token": request.auth}
