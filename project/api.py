5306c3f81ac4d2ca1bb0843ab09e8eb3e8d5509b
Mustafa <mustafa.ezzat@backstage.com>
2025-01-28 01:27:24 +0200

    get project per place

--- project/api.py
+++ project/api.py
@@ -5,6 +5,9 @@ from .schemas import ProjectSchema
 @api_controller('/projects', tags=['Projects'], permissions=[])
 class ProjectController:
     @route.get("/")
-    def list(self):
+    def list(self, place_id: int = None):
         projects = Project.objects.all()
-        return [ProjectSchema.from_orm(p).dict() for p in projects]
+        if place_id is not None:
+            projects = Project.objects.filter(place = place_id)
+        result = [ProjectSchema.from_orm(p).dict() for p in projects]
+        return result
